465a6e9b31f8380d0cd9e0a272450f0955ccffb9
맥 <maeg@maeg-ui-MacBookAir.local>
2025-02-18 13:06:54 +0900

    Add test-html route for debugging

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@
 // Kakao JavaScript SDK가 이미 index.html에서 불러와졌다고 가정합니다.
 
 // Kakao SDK 초기화 (본인의 JavaScript 키로 교체)
-Kakao.init('90ed090fa6d7044e65e276410a0232d7');
+Kakao.init('45df237890d3b3f2f052e30b4977072d');
 console.log("Kakao SDK initialized");
 
 // toPlainObject 함수: Kakao 응답 객체의 모든 속성을 평면 객체로 변환
